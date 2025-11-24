a626de6d0b189eec3c0ff804d218b4f5f4db2aea
davelee2804 <davelee2804@gmail.com>
2025-11-24 11:50:35 +1100

    adding a new lfric-coupled environment for inclusion of the oasis package in a separate container
    Signed-off-by: davelee2804 <davelee2804@gmail.com>

---
+++ environments/lfric-coupled/etc/env.activate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SPACK_ENV_VIEW=$SPACK_ENV/.spack-env/view
+
+export FCM_KEYWORDS=${FCM_KEYWORDS:-$NGMOENVS_ENVDIR/etc/fcm-keyword.cfg}
+export LDMPI=mpifort
+export FPP="cpp -traditional"
+export FFLAGS="${OMPI_FCFLAGS:-} -I$SPACK_ENV_VIEW/include -I$SPACK_ENV_VIEW/lib"
+export LIBRARY_PATH=$SPACK_ENV_VIEW/lib:$LIBRARY_PATH
+
---
+++ tests/envs/lfric-coupled.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "$(readlink -f "${BASH_SOURCE[0]}")" )" &> /dev/null && pwd )
+
+cat <<EOF
+Running lfric_apps test in $BASEDIR
+
+EOF
+
+# Check out lfric source
+[[ -d "$BASEDIR/lfric_apps" ]] || envrun fcm co fcm:lfric_apps.xm/trunk "$BASEDIR/lfric_apps"
+if [[ ! -d "$BASEDIR/lfric_core" ]]; then
+    envrun fcm co fcm:lfric.xm/trunk "$BASEDIR/lfric_core"
+
+    # Fix for recent ifort
+    patch -p0 --forward --directory "$BASEDIR/lfric_core" <<EOF
+--- infrastructure/build/fortran/ifort.mk       (revision 50286)
++++ infrastructure/build/fortran/ifort.mk       (working copy)
+@@ -33,7 +33,7 @@
+ # created. This adds unecessary files to the build so we disable that
+ # behaviour.
+ #
+-FFLAGS_WARNINGS           = -warn all -warn errors -gen-interfaces nosource
++FFLAGS_WARNINGS           = -warn all -gen-interfaces nosource
+ FFLAGS_UNIT_WARNINGS      = -warn all -gen-interfaces nosource
+ FFLAGS_INIT               = -ftrapuv
+EOF
+
+    # Fix for oneapi
+    patch -p0 --forward --directory "$BASEDIR/lfric_core" < "$SCRIPT_DIR/lfric-oneapi.patch"
+fi
+
+APP=gravity_wave
+
+pushd "$BASEDIR"
+
+# Clean any previous build
+rm -rf "$BASEDIR/lfric_apps/applications/$APP/working"
+
+# Build lfric
+envrun lfric_apps/build/local_build.py --application "$APP" --core_source "$BASEDIR/lfric_core"
+
+# Run example
+pushd "$BASEDIR/lfric_apps/applications/$APP/example"
+envrun mpirun -n 6 "../bin/$APP" configuration.nml
+
+cat PET0.*.Log
+
+cat <<EOF
+
+LFRic app $APP successfully ran!
+EOF
